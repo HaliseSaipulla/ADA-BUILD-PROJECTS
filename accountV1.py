577c240a26e31348f2e023d58fef9091c5fec4ad
HaliseSaipulla <halisasaipulla@gmail.com>
2020-09-23 19:27:49 -0700

    V1

---
+++ accountV1.py
@@ -0,0 +1,21 @@
+import random
+#2
+students = ["Halise Saipulla","Adila Abduwali","Dinnare Muhid","Dilmire Dilmurat","Gulgine Erkin"]
+
+#3
+ids = []
+for i in range(5):
+    ids.append(random.randint(1111,9999))
+
+#4
+emails = []
+for student in students:
+   [first,last] = student.split(" ")
+   emails.append(first[0]+last+str(ids[i])[0:3]+"@adadev.or")
+
+
+#5
+for i in range(5):
+    print(f"student: (names[i])")
+    print(f"id:{ids[i]}")
+    print(f"email:{emails[i]}\n")
---
+++ accountV2.py
@@ -0,0 +1,22 @@
+import random 
+#2 
+names = ["Halise Saipulla","Adila Abduwali","Dinnare Muhid","Dilmire Dilmurat","Gulgine Erkin"]
+#creat empty lists
+students =[]
+students_dictio ={}
+
+for i in range(len(names)):
+    #generate id
+    id = (random.randint(1111,9999))
+    #generate email
+    [first,last] = names[i].split(" ")
+    email = first[0] + last + "@adadev.org"
+
+    # create dictionary
+    students_dictio[i] = {"name":names[i], "id":id, "email":email}
+
+    # add each student from dictionary to the sudent list
+    students.append(students_dictio[i])
+    #print(students_dictio)
+
+    print(f"Printing students[{i}]:{students[i]}")
